8f0c3f49ebdefedb2f560ab3339d44673177967a
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-31 19:37:42 +0900

    modi

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
-const Migrations = artifacts.require("Migrations");
+const SampleNFT = artifacts.require("SampleNFT");
 
 module.exports = function (deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(SampleNFT);
 };
