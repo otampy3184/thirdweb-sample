9bd35a6841770257f0ef236ee27a6aced4ddfab1
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-31 19:30:29 +0900

    contract作成とtruffle-conf修正

---
+++ contracts/SampleNFT.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+// openzeppelinのコントラクトをimport
+import "../node_modules/@openzeppelin/contracts/token/erc721/ERC721.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+
+contract SampleNFT is ERC721, Ownable {
+    using Counters for Counters.Coutner;
+    constructor() ERC721("Sapmle", "SMP"){}
+    Counters.Counter private _tokenIdCounter;
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://raw.githubusercontent.com/otampy3184/metadata-okuyo/main/meta/";
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+}
